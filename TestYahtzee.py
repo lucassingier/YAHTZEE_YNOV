4821836a193edcae011fa9b176e4e4ff1c2718dc
lucassingier <lucas.singier@gmail.com>
2022-04-23 10:33:28 +0200

    Troisième commit
    Code: Lucas
    Révision: Basile
    
    Changements: Refactor test et code, renvoi d'un dictionnaire au lieu d'une seule valeur.
                              Passage sur unitTest

--- TestYahtzee.py
+++ TestYahtzee.py
@@ -1,28 +1,49 @@
 import Yahtzee
+from unittest import TestCase, mock, main
 
-class TestClass:
+class TestClass(TestCase):
 
     def test_three_ones_should_return_3(self):
         lancer = [1,1,1,2,5]
-        resultat = Yahtzee.Game().lance(lancer,1)
-        assert resultat ==3
+        resultat = Yahtzee.Game().lance(lancer)
+        for cle, valeur in resultat.items():
+            if cle =="Ones":
+                self.assertEqual(3,valeur)
+                
     def test_four_twos_should_return_8(self):
         lancer = [2,1,2,2,2]
-        resultat = Yahtzee.Game().lance(lancer,2)
-        assert resultat ==8
+        resultat = Yahtzee.Game().lance(lancer)
+        for cle, valeur in resultat.items():
+            if cle =="Twos":
+                self.assertEqual(8,valeur)
+
     def test_two_threes_should_return_6(self):
         lancer = [3,1,3,2,5]
-        resultat = Yahtzee.Game().lance(lancer,3)
-        assert resultat ==6
+        resultat = Yahtzee.Game().lance(lancer)
+        for cle, valeur in resultat.items():
+            if cle =="Threes":
+                self.assertEqual(6,valeur)
+
     def test_five_fours_should_return_20(self):
         lancer = [4,4,4,4,4]
-        resultat = Yahtzee.Game().lance(lancer,4)
-        assert resultat ==20
+        resultat = Yahtzee.Game().lance(lancer)
+        for cle, valeur in resultat.items():
+            if cle =="Fours":
+                self.assertEqual(20,valeur)
+
     def test_two_fives_should_return_10(self):
         lancer = [5,1,1,2,5]
-        resultat = Yahtzee.Game().lance(lancer,5)
-        assert resultat ==11
+        resultat = Yahtzee.Game().lance(lancer)
+        for cle, valeur in resultat.items():
+            if cle =="Fives":
+                self.assertEqual(10,valeur)
+
     def test_three_sixs_should_return_18(self):
         lancer = [6,6,1,6,5]
-        resultat = Yahtzee.Game().lance(lancer,6)
-        assert resultat ==18
+        resultat = Yahtzee.Game().lance(lancer)
+        for cle, valeur in resultat.items():
+            if cle =="Sixes":
+                self.assertEqual(18,valeur)
+
+if __name__ == '__main__':
+  main()
--- Yahtzee.py
+++ Yahtzee.py
@@ -1,12 +1,29 @@
 class Game:
+    diccombo = {
+        "Ones": 1, 
+        "Twos": 2,
+        "Threes": 3, 
+        "Fours": 4, 
+        "Fives": 5, 
+        "Sixes": 6 }
     
-    def lance(self,lancer,num):
+    def lance(self,lancer):
+        resultats ={
+        "Ones": 0, 
+        "Twos": 0,
+        "Threes": 0, 
+        "Fours": 0, 
+        "Fives": 0, 
+        "Sixes": 0}
         cpt=0
         for i in lancer:
-            if (i == num):
-                cpt+=num
-        return cpt
+            for cle, valeur in self.diccombo.items():
+                if i == valeur:
+                    resultats[cle] += i
+        return resultats
+            
+        
 
 game = Game()
-lancer = [2,1,2,2,2]
-print(game.lance(lancer,2))
+lancer = [1,1,1,2,5]
+print(game.lance(lancer))
